58ec322dc5ce885110955c669d2ec7d1b6cd3302
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2024-05-24 22:27:55 +0200

    wip

--- src/pyConvexMap/cell.py
+++ src/pyConvexMap/cell.py
@@ -1,25 +1,28 @@
 import math
 from .point import Point2
+from .segment import Segment
 
 
 class Cell:
 
     # Initialization Destruction:
     def __init__( self, vertices= []):
-        self._vertices= vertices
-        self._tags= [0 for i in range( len(vertices) )]
+        size= len(vertices)
+        self._segments= []
+        if vertices :
+            self._segments= [
+                Segment( v1, v2 )     
+                for v1, v2 in zip( vertices, vertices[1:]+[vertices[0]] )
+            ]
         self.updateCenter()
 
     # Construction:
-    def makeConvex(self):
-        return self._vertices
-    
     def updateCenter(self):
         self._center= Point2(0.0, 0.0)
         weights= 0.0
-        for v1, v2, t in self.segments() :
-            v= v1+v2
-            dist= v1.distance(v2)
+        for s in self.segments() :
+            v= s.pointA() + s.pointB()
+            dist= s.pointA().distance(s.pointB())
             v.scale(0.5)
             self._center.x+= v.x*dist
             self._center.y+= v.y*dist
@@ -28,40 +31,19 @@ class Cell:
             self._center.x/= weights
             self._center.y/= weights
 
-    def add( self, aPoint, aTag ):
-        self._vertices.append( aPoint )
-        self._tags.append( aTag )
-    
-    def setVertice( self, i, aPoint ):
-        self._vertices[i]= aPoint
-    
-    def setVertices( self, vertices ):
-        self._vertices= vertices
+    def add( self, aSegment ):
+        self._segment.append( aSegment )
     
     def setTags( self, tags ):
-        self._tags= tags
-
-    def setTag( self, i, tag ):
-        self._tags[i]= tag
-        
-    def setTag( self, i, tag ):
-        self._tags[i]= tag
+        for s, t in zip( self.segments(), tags ) :
+            s.setTag(t)
     
     # Accessors:
     def size(self):
-        return len( self._vertices )
+        return len( self._segment )
     
     def center(self):
         return self._center
     
-    def vertices(self):
-        return self._vertices
-    
-    def tags(self):
-        return self._tags
-    
     def segments(self):
-        if self.size() > 0 :
-            vertices2= self._vertices[1:] + [self._vertices[0]]
-            return zip( self._vertices, vertices2, self._tags )
-        return zip( [], [], [] )
+        return self._segments
--- tests/test_13_cell.py
+++ tests/test_13_cell.py
@@ -4,7 +4,7 @@ sys.path.insert( 1, __file__.split('tests')[0] )
 # ------------------------------------------------------------------------ #
 #            T E S T   M a r a u B o t M a p : :  B O D Y                  #
 # ------------------------------------------------------------------------ #
-from src.pyConvexMap import Point2, Cell
+from src.pyConvexMap import Point2, Segment, Cell
 
 
 def test_Cell_init():
@@ -15,12 +15,10 @@ def test_Cell_init():
 def test_Cell_init2():
     cell= Cell( [Point2(1, 2), Point2(2, 6), Point2(4, 5), Point2(3, 0)] )
     assert( type(cell) == Cell )
-    assert( cell.vertices() == [Point2(1, 2), Point2(2, 6), Point2(4, 5), Point2(3, 0)] )
-    assert( cell.tags() == [0, 0, 0, 0] )
-    assert( list(cell.segments()) == [
-        (Point2(1, 2), Point2(2, 6), 0),
-        (Point2(2, 6), Point2(4, 5), 0),
-        (Point2(4, 5), Point2(3, 0), 0),
-        (Point2(3, 0), Point2(1, 2), 0)
-    ] )
+    assert( cell.segments() == [
+        Segment( Point2(1, 2), Point2(2, 6) ),
+        Segment( Point2(2, 6), Point2(4, 5) ),
+        Segment( Point2(4, 5), Point2(3, 0) ),
+        Segment( Point2(3, 0), Point2(1, 2) ),
+    ])
     assert( cell.center().round() == Point2(2.5, 3.1) )
